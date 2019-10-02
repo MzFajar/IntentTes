f625479d5fde2d8b07612c7a8db5a85e2eb9cb2f
MzFajar <mzfajar997@gmail.com>
2019-10-02 22:12:19 +0700

    firscommit

---
+++ app/src/main/java/com/example/intenttes/Kontak.java
@@ -0,0 +1,14 @@
+package com.example.intenttes;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Kontak extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.layoutkontak);
+    }
+}
